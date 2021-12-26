0e9930132e8337fe6ba550bb3a1e7687904997f3
ritiksingh2000 <ritikrana370@gmail.com>
2021-12-26 13:55:09 +0530

    Corrections

--- ecom/settings.py
+++ ecom/settings.py
@@ -149,6 +149,8 @@ AWS_ACCESS_KEY_ID = os.environ['AWS_S3_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_S3_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
 AWS_S3_QUERYSTRING_AUTH = False
+AWS_DEFAULT_ACL = None
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
